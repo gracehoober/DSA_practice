490aad7895a930d69d1977738b7eeb9df4f43638
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-12-08 15:49:09 -0800

    Edmonds questions instructions

---
+++ rithm_dsa_list/Edmonds_problem.js
@@ -0,0 +1,10 @@
+/** Write a function that takes a number and prints out each digit of the number
+ * in reverse order.
+ * You cannot change the data type of the string.
+ *
+ *
+ *  I: number
+ *  O: nothing returned from function
+ *
+ * 
+ */
