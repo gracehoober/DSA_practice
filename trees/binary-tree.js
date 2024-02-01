e5467fc8057f26a44f31fb2d0ccd0b3e51efc3ee
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-02-01 10:11:24 -0800

    Tree/binary tree practice set-up

---
+++ trees/binary-tree.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class BinaryTreeNode {
+  constructor(){
+    this.self
+    this.next
+  }
+}
+
+class BinaryTree {
+  constructor(){
+    this.head
+    this.right
+    this.left
+  }
+}
---
+++ trees/tree.js
@@ -0,0 +1,14 @@
+"use strict";
+
+class Tree {
+  constructor(){
+    this.head
+  }
+}
+
+class TreeNode {
+  constructor(){
+    this.self
+    this.next
+  }
+}
