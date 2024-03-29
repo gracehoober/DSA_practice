d25899676e66f308694b91bd0da17114e15d0193
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-29 10:49:33 -0700

    reverse_string passing tests

--- python-ds-practice/05_reverse_string.py
+++ python-ds-practice/05_reverse_string.py
@@ -7,3 +7,7 @@ def reverse_string(phrase):
         >>> reverse_string('sauce')
         'ecuas'
     """
+    phrase_to_list = list(phrase)
+    phrase_to_list.reverse()
+    back_to_phrase = "".join(phrase_to_list)
+    return back_to_phrase
