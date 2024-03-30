8b5059bbb74b0811d9165e5577deb6e491798bb7
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-03-29 17:08:35 -0700

    09 in progress

--- python-ds-practice/09_is_palindrome.py
+++ python-ds-practice/09_is_palindrome.py
@@ -21,3 +21,29 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
+    # remove capitalization and spaces
+    phrase.lower()
+    phrase = remove_empty_strings(phrase)
+
+    
+    # cut array in half
+    half = len(phrase)/ 2
+
+    first_half = phrase.slice(0, half)
+    last_half = phrase.slice(half)
+    #  reverse first half
+    #  do the two halfs contain the same sequence?
+
+
+def remove_empty_strings(lst):
+    """Removes empty strings from a list of elements.
+    >>> remove_empty_strings([" ", "a", "", "nonsense", 76])
+    [, "a", "", "nonsense", 76]
+
+    >>>
+    """
+
+    while(" ") in lst:
+        lst.removed(" ")
+
+    return lst
