500bdf126d291f8c247b95ff5d77d9a8ac951b07
Grace Bethany Hoober <grace.hoober66@gmail.com>
2025-10-08 13:59:06 -0700

    MinHeap pop functionality

--- heaps/python_heap.py
+++ heaps/python_heap.py
@@ -19,9 +19,34 @@ class MinHeap(Heap):
 
         i = len(self.heap)-1
         parent_i = self.parent_i(i)
-        return self.order_heap(i, parent_i)
+        return self.order_heap_push(i, parent_i)
 
-    def order_heap(self, i, parent_i):
+    def pop(self):
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        self.order_heap_pop(0)
+
+        return self.heap.pop()
+
+    def order_heap_pop(self, i):
+        least = i
+        l_i = self.left_child_i(i)
+        r_i = self.right_child_i(i)
+
+        if r_i >= len(self.heap) or l_i >= len(self.heap):
+            return
+
+        if self.heap[l_i] < self.heap[i]:
+            least = l_i
+        if self.heap[r_i] < least:
+            least = r_i
+
+        if least is not i:
+            self.heap[i], self.heap[least] = self.heap[least], self.heap[i]
+            self.order_heap_pop(least)
+        else:
+            return
+
+    def order_heap_push(self, i, parent_i):
         if i == 0:
             return self.heap
 
@@ -37,3 +62,14 @@ class MinHeap(Heap):
 class MaxHeap(Heap):
     def __init__(self):
         super().__init__(min_heap=False)
+
+
+# testing
+min_heap = MinHeap()
+min_heap.push(1)
+min_heap.push(2)
+min_heap.push(4)
+min_heap.push(3)
+x = min_heap.pop()
+len = min_heap.heap
+print(len)
