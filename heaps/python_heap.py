4cb6398db47554f5aac13e1eea6a80c6dfb32bb1
Grace Bethany Hoober <grace.hoober66@gmail.com>
2025-10-08 11:16:56 -0700

    push method on MinHeap

--- heaps/python_heap.py
+++ heaps/python_heap.py
@@ -1,21 +1,37 @@
-# Write the code for a min-heap in Python
 class Heap:
     def __init__(self, min_heap=True):
         self.heap = []
         self.min_heap = min_heap
 
-    def left_child(self, index):
+    def left_child_i(self, index):
         return 2 * index + 1
 
-    def right_child(self, index):
+    def right_child_i(self, index):
         return 2 * index + 2
 
-    def parent(self, index):
-        return (index-1) // 2
+    def parent_i(self, child_i):
+        return (child_i-1) // 2
 
 
 class MinHeap(Heap):
-    pass
+    def push(self, val):
+        self.heap.append(val)
+
+        i = len(self.heap)-1
+        parent_i = self.parent_i(i)
+        return self.order_heap(i, parent_i)
+
+    def order_heap(self, i, parent_i):
+        if i == 0:
+            return self.heap
+
+        if self.heap[i] < self.heap[parent_i]:
+            self.heap[i], self.heap[parent_i] = self.heap[parent_i], self.heap[i]
+            val_i = parent_i
+            parent_i = self.parent_i(val_i)
+            self.order_heap(val_i, parent_i)
+
+        return self.heap
 
 
 class MaxHeap(Heap):
